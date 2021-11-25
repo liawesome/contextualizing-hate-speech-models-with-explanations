5af187822c5dd685e0e453b7915544c09b285434
Li <carriezha321@gmail.com>
2021-11-25 10:06:44 -0800

    Update gab_soc.sh

--- scripts/gab_soc.sh
+++ scripts/gab_soc.sh
@@ -12,7 +12,7 @@ fi
 if [[ -n "$2" ]]; then
     max_seeds=$2
 else
-    max_seeds=3
+    max_seeds=4
 fi
 
 
@@ -22,6 +22,6 @@ echo "reg_strength is ${reg_strength}"
 current_seed=1
 while(( $current_seed < $max_seeds ))
 do
-    python run_model.py --do_train --do_lower_case --data_dir ./data/majority_gab_dataset_25k/ --bert_model bert-base-uncased --max_seq_length 128 --train_batch_size 32 --learning_rate 1e-4 --num_train_epochs 2 --early_stop 5 --output_dir runs/majority_gab_es_reg_nb5_h5_is_bal_pos_seed_${current_seed} --seed ${current_seed} --task_name gab --hiex_add_itself --reg_explanations --nb_range 5 --sample_n 5 --negative_weight 0.1 --reg_strength ${reg_strength}
+    python run_model.py --do_train --do_lower_case --data_dir ./data/majority_gab_dataset_25k/ --bert_model bert-base-uncased --max_seq_length 128 --train_batch_size 32 --learning_rate 2e-4 --num_train_epochs 5 --early_stop 5 --output_dir runs/majority_gab_es_reg_nb5_h5_is_bal_pos_seed_${current_seed} --seed ${current_seed} --task_name gab --hiex_add_itself --reg_explanations --nb_range 5 --sample_n 5 --negative_weight 0.1 --reg_strength ${reg_strength}
     let current_seed++
 done
